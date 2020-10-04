3c4e75f26b14812dc2703d621fa89a18a3d1f881
IndyRoseGee <66797225+IndyRoseGee@users.noreply.github.com>
2020-10-04 17:11:50 +0100

    update open source link

--- src/Footer.js
+++ src/Footer.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./footer.css";
 
 export default function Forecast() {
-    return (
-        <div className="footer">
-            <footer>
-                <a href="https://github.com/IndyRoseGee/Weather-project">
-                    Open-source code
+  return (
+    <div className="footer">
+      <footer>
+        <a href="https://github.com/IndyRoseGee/weather-app-react">
+          Open-source code
         </a>{" "}
         by India Gee
       </footer>
-        </div>
-    );
+    </div>
+  );
 }
